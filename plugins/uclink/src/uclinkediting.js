b9872d9beb97f902b3a4b3e039bf16b1324887ab
Gaurav Jain <gaurav.jain028@gmail.com>
2018-03-22 11:09:39 +0530

    some changes in uclink

--- plugins/uclink/src/uclinkediting.js
+++ plugins/uclink/src/uclinkediting.js
@@ -7,20 +7,15 @@
  * @module link/linkediting
  */
 
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import LinkEditing from '@ckeditor/ckeditor5-link/src/linkediting';
 import {
-	downcastAttributeToElement,
-	downcastMarkerToHighlight,
-	createViewElementFromHighlightDescriptor
+	downcastAttributeToElement
 } from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
 import { upcastElementToAttribute } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
 import LinkCommand from './uclinkcommand';
 import UnlinkCommand from './ucunlinkcommand';
 import { createLinkElement } from '@ckeditor/ckeditor5-link/src/utils';
 import bindTwoStepCaretToAttribute from '@ckeditor/ckeditor5-engine/src/utils/bindtwostepcarettoattribute';
-import findLinkRange from '@ckeditor/ckeditor5-link/src/findlinkrange';
-import DocumentSelection from '@ckeditor/ckeditor5-engine/src/model/documentselection';
-import ModelSelection from '@ckeditor/ckeditor5-engine/src/model/selection';
 
 /**
  * The link engine feature.
@@ -29,7 +24,7 @@ import ModelSelection from '@ckeditor/ckeditor5-engine/src/model/selection';
  *
  * @extends module:core/plugin~Plugin
  */
-export default class UcLinkEditing extends Plugin {
+export default class UcLinkEditing extends LinkEditing {
 	/**
 	 * @inheritDoc
 	 */
@@ -65,74 +60,5 @@ export default class UcLinkEditing extends Plugin {
 
 		// Setup highlight over selected link.
 		this._setupLinkHighlight();
-	}
-
-	/**
-	 * Adds highlight over link which has selection inside, together with two-step caret movement indicates whenever
-	 * user is typing inside the link.
-	 *
-	 * @private
-	 */
-	_setupLinkHighlight() {
-		const editor = this.editor;
-		const model = this.editor.model;
-		const doc = model.document;
-		const highlightDescriptor = {
-			id: 'linkBoundaries',
-			class: 'ck-link_selected',
-			priority: 1
-		};
-
-		// Convert linkBoundaries marker to view highlight.
-		editor.conversion.for( 'editingDowncast' )
-			.add( downcastMarkerToHighlight( {
-				model: 'linkBoundaries',
-				view: highlightDescriptor
-			} ) );
-
-		// Create marker over whole link when selection has "linkHref" attribute.
-		doc.registerPostFixer( writer => {
-			const selection = doc.selection;
-			const marker = model.markers.get( 'linkBoundaries' );
-
-			// Create marker over link when selection is inside or remove marker otherwise.
-			if ( selection.hasAttribute( 'linkHref' ) ) {
-				const modelRange = findLinkRange( selection.getFirstPosition(), selection.getAttribute( 'linkHref' ) );
-
-				if ( !marker || !marker.getRange().isEqual( modelRange ) ) {
-					writer.setMarker( 'linkBoundaries', modelRange );
-					return true;
-				}
-			} else if ( marker ) {
-				writer.removeMarker( 'linkBoundaries' );
-				return true;
-			}
-
-			return false;
-		} );
-
-		// Custom converter for selection's "linkHref" attribute - when collapsed selection has this attribute it is
-		// wrapped with <span> similar to that used by highlighting mechanism. This <span> will be merged together with
-		// highlight wrapper. This prevents link splitting When selection is at the beginning or at the end of the link.
-		// Without this converter:
-		//
-		//		<a href="url">{}</a><span class="ck-link_selected"><a href="url">foo</a></span>
-		//
-		// When converter is applied:
-		//
-		//		<span class="ck-link_selected"><a href="url">{}foo</a></span>
-		editor.editing.downcastDispatcher.on( 'attribute:linkHref', ( evt, data, conversionApi ) => {
-			const selection = data.item;
-
-			if ( !( selection instanceof DocumentSelection || selection instanceof ModelSelection ) || !selection.isCollapsed ) {
-				return;
-			}
-
-			const writer = conversionApi.writer;
-			const viewSelection = writer.document.selection;
-			const wrapper = createViewElementFromHighlightDescriptor( highlightDescriptor );
-
-			conversionApi.writer.wrap( viewSelection.getFirstRange(), wrapper );
-		} );
 	}
 }
